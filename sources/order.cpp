eadb08f4b85b0e341f711a25157343e59c132c8d
Thishaan <udia@wisc.edu>
2026-06-08 13:08:30 -0500

    added new order type

--- sources/order.cpp
+++ sources/order.cpp
@@ -2,7 +2,7 @@
 
 #include "../headers/Order.h"
 
-Order::Order(Type type, Status status,
+Order::Order(Order::Order_Type type, Status status,
              std::uint64_t order_ID, float price, int quantity, int filled_quantity)
     : type{type}, status{status}, order_ID{order_ID}, price{price}, quantity{quantity}, filled_quantity{filled_quantity} {}
 
